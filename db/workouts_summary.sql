08939234b3c2f7194191e799302e148153bc796c
Michael Garvin <gar+gh@danger.computer>
2016-08-16 22:53:50 -0700

    add activity count to workout summaries

--- db/workouts_summary.sql
+++ db/workouts_summary.sql
@@ -1,4 +1,8 @@
-SELECT id, name, to_char(date, 'YYYY-MM-DD') as date
-FROM WORKOUTS
+SELECT
+  workouts.id,
+  name,
+  to_char(workouts.date, 'YYYY-MM-DD') as date,
+  jsonb_array_length(workouts.activities) as activities
+FROM workouts
 WHERE user_id = ${user_id}
-
+GROUP BY workouts.id
