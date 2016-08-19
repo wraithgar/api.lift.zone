3041adb0276cc93d5381e2c85f50ec932af76574
Michael Garvin <gar+gh@danger.computer>
2016-08-19 16:01:40 -0700

    fix suggestions query

--- db/activities_search.sql
+++ db/activities_search.sql
@@ -4,11 +4,11 @@ SELECT
   ELSE activity_id
   END as activity_id,
   name,
-  1 - ts_rank(to_tsvector('english', name), ${name}) as rank
+  1 - ts_rank(to_tsvector('english', name), to_tsquery(${name})) as rank
 FROM activities
 WHERE
   user_id = ${user_id}
   AND
-  to_tsvector('english', name) @@ ${name}
+  to_tsvector('english', name) @@ to_tsquery(${name})
 ORDER BY rank DESC
 LIMIT 5
