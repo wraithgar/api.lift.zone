5cddc38e72e318d3c33892b67d1975cbe77a5e74
Michael Garvin <gar+gh@danger.computer>
2016-09-01 09:06:27 -0700

    add unique lower email constraint to users table

--- migrations/20160829183457_lower_case_email.js
+++ migrations/20160829183457_lower_case_email.js
@@ -4,14 +4,14 @@ exports.up = function(knex, Promise) {
 
   return Promise.all([
     knex.raw('DROP INDEX IF EXISTS "users_email_index"'),
-    knex.raw('CREATE INDEX "users_email_lowercase_index" ON users ((lower(email)))')
+    knex.raw('CREATE UNIQUE INDEX users_email_lower_unique on users ((lower(email)))')
   ]);
 };
 
 exports.down = function(knex) {
 
   return Promise.all([
-    knex.raw('DROP INDEX IF EXISTS "users_email_lowercase_index"'),
+    knex.raw('DROP INDEX IF EXISTS "users_email_lower_unique"'),
     knex.raw('CREATE INDEX "users_email_index" ON users (email)')
   ]);
 };
