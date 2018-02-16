aad9d1f2f8ed58760f265237ffe39753d101d661
Michael Garvin <gar+gh@danger.computer>
2018-02-16 09:11:27 -0800

    change info returned from admin/users
    Don't include hash, add most recent workout date

--- db/users_summary.sql
+++ db/users_summary.sql
@@ -1,6 +1,11 @@
 SELECT
-  users.*,
+  users.id,
+  users.name,
+  users.email,
+  users.active,
+  users.validated,
   (SELECT count(workouts.id)::integer from workouts where workouts.user_id = users.id) as workouts,
   (SELECT count(activities.id)::integer from activities where activities.user_id = users.id) as activities,
-  (SELECT count(invites.token)::integer from invites where invites.user_id = users.id and invites.claimed_by is null) as invites
+  (SELECT count(invites.token)::integer from invites where invites.user_id = users.id and invites.claimed_by is null) as invites,
+  (SELECT max(workouts.created_at) from workouts where workouts.user_id = users.id) as last_workout
 FROM users
