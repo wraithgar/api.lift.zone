e19d17356e69b810c7f78ce93c8c96007b6820a0
Michael Garvin <gar+gh@danger.computer>
2019-06-26 09:04:42 -0700

    fix(migrations): use native Promise in all migrations
    switch to async for/of loop where nonstandard Promise.map was being used

--- migrations/20160819191626_workout_raw_date.js
+++ migrations/20160819191626_workout_raw_date.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
 
   return knex.schema.table('workouts', (table) => {
 
@@ -8,7 +8,7 @@ exports.up = function(knex, Promise) {
   });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
 
   return knex.schema.table('workouts', (table) => {
 
--- migrations/20170207104641_user_preferences_unit.js
+++ migrations/20170207104641_user_preferences_unit.js
@@ -1,28 +1,28 @@
 'use strict';
-exports.up = function(knex, Promise) {
+exports.up = function (knex) {
 
-  return knex.table('users').select('id', 'preferences').then((users) => {
+  return knex.table('users').select('id', 'preferences').then(async (users) => {
 
-    return Promise.map(users, (user) => {
+    for (const user of users) {
 
       user.preferences.weightUnit = 'lb';
-      return knex.table('users').where({ id: user.id }).update({ preferences: user.preferences });
-    });
+      await knex.table('users').where({ id: user.id }).update({ preferences: user.preferences });
+    }
   }).then(() => {
 
     return knex.raw(`ALTER TABLE ONLY users ALTER COLUMN preferences SET DEFAULT '${JSON.stringify({ weightUnit: 'lb', smartmode: true, visible: false, dateFormat: 'dddd, MMM Do YYYY' })}'`);
   });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex) {
 
-  return knex.table('users').select('id', 'preferences').then((users) => {
+  return knex.table('users').select('id', 'preferences').then(async (users) => {
 
-    return Promise.map(users, (user) => {
+    for (const user of users) {
 
       delete user.preferences.weightUnit;
-      return knex.table('users').where({ id: user.id }).update({ preferences: user.preferences });
-    });
+      await knex.table('users').where({ id: user.id }).update({ preferences: user.preferences });
+    }
   }).then(() => {
 
     return knex.raw(`ALTER TABLE ONLY users ALTER COLUMN preferences SET DEFAULT '${JSON.stringify({ smartmode: true, visible: false, dateFormat: 'dddd, MMM Do YYYY' })}'`);
